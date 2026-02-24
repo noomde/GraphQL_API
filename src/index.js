fb7cc97d12d7add62e8cb508c36529535fdc26d4
Anthon Bäckström <ab226rf@student.lnu.se>
2026-02-24 15:29:09 +0100

    stanholder for server and database configuration.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// route/graphQL
+
+// server listen
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
