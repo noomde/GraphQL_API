7b05c1b04e68da230b8ed542f5e0b7605535f069
Anthon Bäckström <ab226rf@student.lnu.se>
2026-02-26 14:42:07 +0100

    Added simnple

---
+++ src/resolvers/usersResolver.js
@@ -0,0 +1,28 @@
+import { UsersController } from '../controller/usersController';
+
+export default {
+  Mutation: {
+    /**
+     * Registers a new user.
+     *
+     * @param {*} _ - The parent object.
+     * @param {*} userData - The data for the user to be registered.
+     * @returns {Promise<Object>} The new user object.
+     */
+    registerUser: async (_, { userData }) => {
+      return await UsersController.registerUser(userData);
+    },
+
+    /**
+     * Logs in a user.
+     *
+     * @param {*} _ - The parent object.
+     * @param {*} username - The username.
+     * @param {*} password - The password.
+     * @returns {Promise<Object>} The user object.
+     */
+    loginUser: async (_, { username, password }) => {
+      return await UsersController.loginUser(username, password);
+    },
+  },
+};
