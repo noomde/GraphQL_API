3bc84ece1f4e31dd41f2017e849ef939a8769c37
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-15 14:57:39 +0200

    Added statistics controller

---
+++ src/controller/statisticsController.js
@@ -0,0 +1,44 @@
+import { StatisticsRepository } from '../repository/statisticsRepository.js';
+
+/**
+ * Resposible for handling business logic for statistics.
+ */
+export class StatisticsController {
+  #statisticsRepository;
+
+  /**
+   * Creats an instance of statisticsController.
+   *
+   * @param {StatisticsRepository} statisticsRepository  - The repository for handling statistics data interactions.
+   */
+  constructor(statisticsRepository = new StatisticsRepository()) {
+    this.#statisticsRepository = statisticsRepository;
+  }
+
+  /**
+    Gets the average score per platform
+   *
+   * @returns {Promise<object[]>} - Platform statistics.
+   */
+  async getAverageScorePerPlatform() {
+    return await this.#statisticsRepository.findAverageScorePerPlatform();
+  }
+
+  /**
+   * Gets the average score per developer.
+   *
+   * @returns {Promise<object[]>} - Developer statistics.
+   */
+  async getAverageScorePerDeveloper() {
+    return await this.#statisticsRepository.findAverageScorePerDeveloper();
+  }
+
+  /**
+   * Gets the average score per publisher.
+   *
+   * @returns {Promise<object[]>} - Publisher statistics.
+   */
+  async getAverageScorePerPublisher() {
+    return await this.#statisticsRepository.findAverageScorePerPublisher();
+  }
+}
